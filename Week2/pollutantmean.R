b8519ed207e966b28602b9ffb9b2178002b28781
Francesca Montesi <francesca.montesi00@gmail.com>
2020-05-13 13:21:38 +0200

    Reorganised directories

---
+++ Week2/pollutantmean.R
@@ -0,0 +1,18 @@
+
+pollutantmean <- function(directory, pollutant, id= 1:322){
+  #obtain list of sensor files in specdata directory
+    # create empty data frame
+    
+    #subset list of sensor files
+    
+    #loop through files in subset list and
+    #     * read the csv file
+    #     * binf the "collector" data frame
+    
+    #calculate mean and return to parent environment
+ 
+
+  
+}
+
+
