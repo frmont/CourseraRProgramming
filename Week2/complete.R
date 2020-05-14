4581a1cbbe9f3b02be33fca954145789700dccbc
Francesca Montesi <francesca.montesi00@gmail.com>
2020-05-14 18:34:38 +0200

    Created empty object and put rbind using d, created the dataframe inside rbind

--- Week2/complete.R
+++ Week2/complete.R
@@ -2,13 +2,17 @@ setwd("~/Desktop/GoogleDrive/R_Programming/CourseraRProgramming/Week2")
 complete <- function(directory, id) {
  
   allfiles <- list.files(directory, full.names=TRUE)
+  d=NULL
+   # create empty dataframe
   for (i in id) {
     
     numcomplcases <- sum(complete.cases(read.csv(allfiles[i]))) # is.numeric(numcomplcases) [1] TRUE
-    dat <- data.frame(id = i, nobs = numcomplcases, stringsAsFactors = FALSE) # create empty dataframe
-    x <- rbind(dat)
+    d = rbind(d, data.frame(id = i, nobs = numcomplcases, stringsAsFactors = FALSE))
+  
   }
-  print(x)
+  print(d)
 }
 complete("specdata", 1)
-
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
