8d8de3d50d9e45b5178c3911df9ab4dd904ebcd7
Francesca Montesi <francesca.montesi00@gmail.com>
2020-05-14 11:23:51 +0200

    First rough writing of code

--- Week2/pollutantmean.R
+++ Week2/pollutantmean.R
@@ -1,18 +1,13 @@
-
-pollutantmean <- function(directory, pollutant, id= 1:322){
-  #obtain list of sensor files in specdata directory
-    # create empty data frame
-    
-    #subset list of sensor files
-    
-    #loop through files in subset list and
-    #     * read the csv file
-    #     * binf the "collector" data frame
-    
-    #calculate mean and return to parent environment
- 
-
-  
-}
+pollutantmean <- function(directory = "specdata", pollutant, id= 1:322){
+    message(paste("Working directory is: ",getwd()))
+    allfiles <- list.files(getwd(), full.names=TRUE)        #obtain list of sensor files in specdata directory
+    files <- data.frame()      # create empty data frame
+    for (i in id) {            #loop through files in subset list and
+      files <- rbind(allfiles, read.csv(allfiles[i]))      # subset list of sensor files
+       }                                                   # read the csv file                                                         
+                                                           # bind the "collector" data frame
+    mean(files[[pollutant]], na.rm=TRUE)  #calculate mean and return to parent environment
+                                     #double square brackets->numeric vector, needed by "mean" (no a data frame)
+    }
 
 
