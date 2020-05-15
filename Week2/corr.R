c738f0430724e6371ecd6782b77d68ff6607630d
Francesca Montesi <francesca.montesi00@gmail.com>
2020-05-16 01:13:53 +0200

    Threshold, subsetting and sulf and nit

--- Week2/corr.R
+++ Week2/corr.R
@@ -2,33 +2,24 @@
 # and calculates the correlation between sulfate and nitrate for monitor locations 
 # where the number of completely observed cases (on all variables) is greater than the threshold.
 
-corr <- function(directory, threshold=0){ #the dafault is 0
+corr <- function(directory, threshold=0){                 # the dafault is 0
   setwd("~/Desktop/GoogleDrive/R_Programming/CourseraRProgramming/Week2")
   source("complete.R")
   compl <- complete(directory="specdata", 1:332)
   
-  for (i in 1:322) {
-    numcomplcases[ numcompletecases < threshold ] <- 0
-  }
-  abovethr <- subset(compl, nobs!="0")    #now we have a list of the ids where nobs > threshold
-  print(abovethr)
-
-  allfiles <- list.files(directory, full.names=TRUE)  
-  dat <- data.frame()                                
-  dat<- rbind(dat, read.csv(allfiles))
-  print(dat)
+  compl %>% filter(nobs > threshold)                       # list of the nobs > threshold
   
-  dat %>%     
-    filter(id %in% abovethr$id)     # only take the columns of dat containing the id in abovethr
-  
-  
-    sulf <- dplyr::pull(dat, sulfate)
-    nit <- dplyr::pull(dat, nitrate)
+  allfiles <- list.files("specdata", full.names=TRUE)
+  fullfiles<- do.call("rbind", lapply(allfiles, read.csv))
+                                                                
+  sub <- data.frame()             
+  sub <- na.omit(subset(fullfiles, id %in% compl$id))   # only take the columns of allfiles containing the id in compl
+                                                        # remove rows with at least one NA
+  sulf <- dplyr::pull(sub, sulfate)
+  nit <- dplyr::pull(sub, nitrate)
     
-    
-    
-    cov(sul, nit) 
+    cov(sulf, nit) 
 
 }
 cr <- corr("specdata", 150)
-
+head(cr)
